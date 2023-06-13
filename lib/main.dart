116ec383f5a3a30fe014748ee77ab0319a67a880
kartikpachori <kartikeypachori1610@gmail.com>
2023-06-13 23:44:59 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'MyShop',
         theme: ThemeData(
-          primarySwatch: Colors.purple,
+          primarySwatch: Colors.blueGrey,
           accentColor: Colors.deepOrange,
           fontFamily: 'Lato',
           appBarTheme: AppBarTheme(
