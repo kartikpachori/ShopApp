2e89506aedfafac21bfad012db7578e245cf1237
kartikpachori <kartikeypachori1610@gmail.com>
2023-06-30 17:02:24 +0530

    006 _Edit Product_ Screen _

---
+++ lib/screens/edit_product_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class EditProductScreen extends StatefulWidget {
+  @override
+  State<EditProductScreen> createState() => _EditProductScreenState();
+}
+
+class _EditProductScreenState extends State<EditProductScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Edit Product'),
+      ),
+    );
+  }
+}
