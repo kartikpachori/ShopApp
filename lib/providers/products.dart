fca956e8b78661ecc5b3a515f7a81003cc1d338a
kartikpachori <kartikeypachori1610@gmail.com>
2023-07-09 16:47:19 +0530

    014 How to Transform Fetched Data.

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -6,86 +6,86 @@ import 'package:http/http.dart' as http;
 
 class Products with ChangeNotifier {
   List<Product> _items = [
-    Product(
-      id: 'p1',
-      title: 'T-Shirt',
-      description: 'A red t-shirt for men',
-      price: 250.00,
-      imageUrl:
-          'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
-    ),
-    Product(
-      id: 'p2',
-      title: 'Trousers',
-      description: 'A nice pair of trousers.',
-      price: 200.00,
-      imageUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Trousers%2C_dress_%28AM_1960.022-8%29.jpg/512px-Trousers%2C_dress_%28AM_1960.022-8%29.jpg',
-    ),
-    Product(
-      id: 'p3',
-      title: ' Scarf',
-      description: 'Warm and cozy - exactly what you need for the winter.',
-      price: 150.00,
-      imageUrl:
-          'https://live.staticflickr.com/4043/4438260868_cc79b3369d_z.jpg',
-    ),
-    Product(
-      id: 'p4',
-      title: 'Pan',
-      description: 'Prepare any meal you want.',
-      price: 100.00,
-      imageUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
-    ),
-    Product(
-      id: 'p5',
-      title: 'Football',
-      description: 'Stylish football for footballers',
-      price: 500.00,
-      imageUrl:
-          'https://m0.sportsjoe.ie/wp-content/uploads/2016/10/29101120/GettyImages-617343966-1024x683.jpg',
-    ),
-    Product(
-      id: 'p6',
-      title: 'Skirt',
-      description: 'Beautiful skirts for girls',
-      price: 500.00,
-      imageUrl:
-          'https://i.pinimg.com/736x/e1/aa/bd/e1aabded945ca5a789d3306859504ce5.jpg',
-    ),
-    Product(
-      id: 'p7',
-      title: 'Earings',
-      description: 'Glamorous earings',
-      price: 200.00,
-      imageUrl:
-          'https://images-na.ssl-images-amazon.com/images/I/71MKCikiIoL._UL1500_.jpg',
-    ),
-    Product(
-      id: 'p8',
-      title: 'Kurta',
-      description: 'kurta for mens ',
-      price: 800.00,
-      imageUrl:
-          'https://i.etsystatic.com/22388648/r/il/9e3f45/2195667858/il_fullxfull.2195667858_3asm.jpg',
-    ),
-    Product(
-      id: 'p9',
-      title: 'Watch',
-      description: 'Stylish pair of watches',
-      price: 1200.00,
-      imageUrl:
-          'https://images.pexels.com/photos/1697218/pexels-photo-1697218.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    ),
-    Product(
-      id: 'p10',
-      title: 'Shoes',
-      description: 'Good looking pair of shoes',
-      price: 1500.00,
-      imageUrl:
-          'https://images.pexels.com/photos/2529157/pexels-photo-2529157.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    ),
+    // Product(
+    //   id: 'p1',
+    //   title: 'T-Shirt',
+    //   description: 'A red t-shirt for men',
+    //   price: 250.00,
+    //   imageUrl:
+    //       'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
+    // ),
+    // Product(
+    //   id: 'p2',
+    //   title: 'Trousers',
+    //   description: 'A nice pair of trousers.',
+    //   price: 200.00,
+    //   imageUrl:
+    //       'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Trousers%2C_dress_%28AM_1960.022-8%29.jpg/512px-Trousers%2C_dress_%28AM_1960.022-8%29.jpg',
+    // ),
+    // Product(
+    //   id: 'p3',
+    //   title: ' Scarf',
+    //   description: 'Warm and cozy - exactly what you need for the winter.',
+    //   price: 150.00,
+    //   imageUrl:
+    //       'https://live.staticflickr.com/4043/4438260868_cc79b3369d_z.jpg',
+    // ),
+    // Product(
+    //   id: 'p4',
+    //   title: 'Pan',
+    //   description: 'Prepare any meal you want.',
+    //   price: 100.00,
+    //   imageUrl:
+    //       'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
+    // ),
+    // Product(
+    //   id: 'p5',
+    //   title: 'Football',
+    //   description: 'Stylish football for footballers',
+    //   price: 500.00,
+    //   imageUrl:
+    //       'https://m0.sportsjoe.ie/wp-content/uploads/2016/10/29101120/GettyImages-617343966-1024x683.jpg',
+    // ),
+    // Product(
+    //   id: 'p6',
+    //   title: 'Skirt',
+    //   description: 'Beautiful skirts for girls',
+    //   price: 500.00,
+    //   imageUrl:
+    //       'https://i.pinimg.com/736x/e1/aa/bd/e1aabded945ca5a789d3306859504ce5.jpg',
+    // ),
+    // Product(
+    //   id: 'p7',
+    //   title: 'Earings',
+    //   description: 'Glamorous earings',
+    //   price: 200.00,
+    //   imageUrl:
+    //       'https://images-na.ssl-images-amazon.com/images/I/71MKCikiIoL._UL1500_.jpg',
+    // ),
+    // Product(
+    //   id: 'p8',
+    //   title: 'Kurta',
+    //   description: 'kurta for mens ',
+    //   price: 800.00,
+    //   imageUrl:
+    //       'https://i.etsystatic.com/22388648/r/il/9e3f45/2195667858/il_fullxfull.2195667858_3asm.jpg',
+    // ),
+    // Product(
+    //   id: 'p9',
+    //   title: 'Watch',
+    //   description: 'Stylish pair of watches',
+    //   price: 1200.00,
+    //   imageUrl:
+    //       'https://images.pexels.com/photos/1697218/pexels-photo-1697218.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    // ),
+    // Product(
+    //   id: 'p10',
+    //   title: 'Shoes',
+    //   description: 'Good looking pair of shoes',
+    //   price: 1500.00,
+    //   imageUrl:
+    //       'https://images.pexels.com/photos/2529157/pexels-photo-2529157.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    // ),
   ];
 
   List<Product> get items {
@@ -105,7 +105,20 @@ class Products with ChangeNotifier {
         'new-project-72be3-default-rtdb.firebaseio.com', '/products.json');
     try {
       final response = await http.get(url);
-      print(json.decode(response.body));
+      final extractedData = json.decode(response.body) as Map<String, dynamic>;
+      final List<Product> loadedProducts = [];
+      extractedData.forEach((prodId, prodData) {
+        loadedProducts.add(Product(
+          id: prodId,
+          title: prodData['title'],
+          description: prodData['description'],
+          price: prodData['price'],
+          isFavorite: prodData['isFavorite'],
+          imageUrl: prodData['imageUrl'],
+        ));
+      });
+      _items = loadedProducts;
+      notifyListeners();
     } catch (error) {
       throw (error);
     }
--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -24,11 +24,20 @@ class ProductsOverviewScreen extends StatefulWidget {
 class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
   var _showOnlyFavorites = false;
   var _isInit = true;
+  var _isLoading = false;
 
   @override
   void didChangeDependencies() {
     if (_isInit) {
-      Provider.of<Products>(context).fetchAndSetProducts();
+      setState(() {
+        _isLoading = true;
+      });
+
+      Provider.of<Products>(context).fetchAndSetProducts().then((_) {
+        setState(() {
+          _isLoading = false;
+        });
+      });
     }
     _isInit = false;
     super.didChangeDependencies();
@@ -37,57 +46,60 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 197, 233, 249),
-      appBar: AppBar(
-        title: Text(
-          'E Shop',
-          style: TextStyle(
-            color: Colors.black,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        actions: <Widget>[
-          PopupMenuButton(
-            onSelected: (FilterOptions selectedValue) {
-              setState(() {
-                if (selectedValue == FilterOptions.Favorites) {
-                  _showOnlyFavorites = true;
-                } else {
-                  _showOnlyFavorites = false;
-                }
-                print(selectedValue);
-              });
-            },
-            icon: Icon(
-              Icons.more_vert,
-              color: Color.fromARGB(183, 255, 255, 255),
+        backgroundColor: Color.fromARGB(255, 197, 233, 249),
+        appBar: AppBar(
+          title: Text(
+            'E Shop',
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
             ),
-            itemBuilder: (_) => [
-              PopupMenuItem(
-                  child: Text('Only Favorites'),
-                  value: FilterOptions.Favorites),
-              PopupMenuItem(child: Text('Show All'), value: FilterOptions.All),
-            ],
           ),
-          Consumer<Cart>(
-            builder: (_, cart, ch) => Badge(
-              child: IconButton(
-                icon: Icon(
-                  Icons.shopping_cart,
-                  color: Color.fromARGB(183, 255, 255, 255),
-                ),
-                onPressed: () {
-                  Navigator.of(context).pushNamed(CartScreen.routeName);
-                },
+          actions: <Widget>[
+            PopupMenuButton(
+              onSelected: (FilterOptions selectedValue) {
+                setState(() {
+                  if (selectedValue == FilterOptions.Favorites) {
+                    _showOnlyFavorites = true;
+                  } else {
+                    _showOnlyFavorites = false;
+                  }
+                  print(selectedValue);
+                });
+              },
+              icon: Icon(
+                Icons.more_vert,
+                color: Color.fromARGB(183, 255, 255, 255),
               ),
-              value: cart.itemCount.toString(),
+              itemBuilder: (_) => [
+                PopupMenuItem(
+                    child: Text('Only Favorites'),
+                    value: FilterOptions.Favorites),
+                PopupMenuItem(
+                    child: Text('Show All'), value: FilterOptions.All),
+              ],
             ),
-          ),
-        ],
-      ),
-      drawer: AppDrawer(),
-      body: ProductsGrid(_showOnlyFavorites),
-      //body: ProductsGrid(),
-    );
+            Consumer<Cart>(
+              builder: (_, cart, ch) => Badge(
+                child: IconButton(
+                  icon: Icon(
+                    Icons.shopping_cart,
+                    color: Color.fromARGB(183, 255, 255, 255),
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).pushNamed(CartScreen.routeName);
+                  },
+                ),
+                value: cart.itemCount.toString(),
+              ),
+            ),
+          ],
+        ),
+        drawer: AppDrawer(),
+        body: _isLoading
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : ProductsGrid(_showOnlyFavorites));
   }
 }
